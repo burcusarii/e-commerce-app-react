6ab00c1c0b8ac1ea1acb8b462ea8b402b69ebbe5
burcu irem sarı <burcu.sari5334@gmail.com>
2023-07-11 23:20:28 +0300

    navbar added

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 function Navbar() {
   return (
-    <div>
-      <nav>
-        <ul>
+    <nav className={styles.nav}>
+      <div className={styles.left}>
+        <div className="logo">
+          <Link to={"/"}>E-Commerce</Link>
+        </div>
+        <ul className={styles.menu}>
           <li>
-            <Link to={"/"}>Home</Link>
+            <Link to={"/products"}>Products</Link>
           </li>
           <li>
             <Link to={"about"}>About</Link>
           </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+      <div className={styles.right}>
+        <Link to={"/signin"}>
+          <Button colorScheme="blue">Login</Button>
+        </Link>
+        <Link to={"/signup"}>
+          <Button colorScheme="blue">Register</Button>
+        </Link>
+      </div>
+    </nav>
   );
 }
 
